9b274b345a67f89a30571dd85cacb4fe0a692732
AJ <anto296@hotmail.com>
2022-03-04 20:23:13 +0100

    problem 1B

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from problem import *
 from temporal_difference import *
 
 colors = ['red', 'green', 'blue', 'violet']
-def get_plots(ax, V_problem, title, trans, index):
+def get_plots_A(ax, V_problem, title, trans, index):
     ax.set_xlabel('State')
     ax.set_ylabel('V (s)')
     ax.set_title(title, fontsize=24, loc="center", pad=10)
@@ -21,34 +21,48 @@ def solution_1A():
     fig, ax = subplots()
     for i,exp in enumerate(exponent):
         V_lazy = TD0(lazy_problem, 10**exp)
-        get_plots(ax,V_lazy,'Lazy',10**exp, i)
+        get_plots_A(ax,V_lazy,'Lazy',10**exp, i)
 
     fig.savefig('V_TD0_lazy.png')
 
     fig, ax = subplots()
     for i,exp in enumerate(exponent):
         V_lazy = TD0(agg_problem, 10**exp)
-        get_plots(ax,V_lazy,'Aggresive',10**exp, i)
+        get_plots_A(ax,V_lazy,'Aggresive',10**exp, i)
 
     fig.savefig('V_TD0_agg.png')
 
-solution_1A()
+def get_plots_B(ax, V_problem, title, fm, index):
+    ax.set_xlabel('State')
+    ax.set_ylabel('V (s)')
+    ax.set_title(title, fontsize=24, loc="center", pad=10)
+    ax.scatter(
+        range(0, len(V_problem)), V_problem, 
+        marker='.', color=colors[index], 
+        label='Feature map : '+ fm
+    )
+    ax.legend()
+    return ax
 
 def solution_1B():
     f_map_list = [f_map_fine,f_map_coarse, f_map_pwl]
     f_map_name =['Fine','Coarse', 'PWL']
 
+    fig, ax = subplots()
     for i, f_map in enumerate(f_map_list):
         V_lazy = LSTD(lazy_problem,f_map_fine, 1e5)
-        print(f_map_name[i])
-        plot(V_lazy,'go-',label='Feature map: '+ f_map_name[i])
-        # savefig('V_TSTD_'+f_map_name[i]+'_lazy.png')
-        show()
+        get_plots_B(ax,V_lazy,'Lazy',f_map_name[i], i)
 
-    for i, f_map in enumerate(f_map_list):  
-        V_agg = LSTD(agg_problem,f_map, 1e5)
-        plot(V_agg,'go-',label='Feature map: '+ f_map_name[i])
-        # savefig('V_TSTD_'+f_map_name[i]+'_lazy.png')
-        show()
+    fig.savefig('V_LSTD_lazy.png')
 
-# solution_1B()
+    fig, ax = subplots()
+    for i, f_map in enumerate(f_map_list):
+        V_agg = LSTD(agg_problem,f_map_fine, 1e5)
+        get_plots_B(ax,V_agg,'Agg',f_map_name[i], i)
+
+    fig.savefig('V_LSTD_agg.png')
+
+
+if __name__ == '__main__':
+    solution_1A()
+    solution_1B()
